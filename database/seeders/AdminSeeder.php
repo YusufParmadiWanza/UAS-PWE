a7c7b24e7275702328993d6592394c3207ddf06b
YusufParmadiWanza <yusufyf2324@gmail.com>
2025-06-02 01:33:09 +0700

    fiks banget

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -10,11 +10,13 @@ class AdminSeeder extends Seeder
 {
     public function run(): void
     {
-        User::create([
-            'name' => 'Admin',
-            'email' => 'admin@admin.com',
-            'password' => Hash::make('password'),
-            'is_admin' => true,
-        ]);
+        User::updateOrCreate(
+            ['email' => 'admin@admin.com'],
+            [
+                'name' => 'Admin',
+                'password' => Hash::make('password'),
+                'is_admin' => true,
+            ]
+        );
     }
-} 
+}
